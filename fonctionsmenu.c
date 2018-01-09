cb94055bd554f3b32e93d8025e700ed613806209
Jrbdxa <34715106+Jrbdxa@users.noreply.github.com>
2018-01-09 17:03:11 +0100

    Update fonctionsmenu.c

--- fonctionsmenu.c
+++ fonctionsmenu.c
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-#include "headermenu.h"
+#include "header_menu.h"
 
 int printMainMenu() {
     int ch1=0;
@@ -29,7 +29,7 @@ int choiceMainMenu(joueur rep[], int id) {
                 break;
             
             case 3:
-                printf("En développement");
+                tabScores(rep, id);
                 break;
             
             case 4:
@@ -61,19 +61,19 @@ int choiceGameMenu(joueur rep[], int id) {
         ch2=printGameMenu();
         switch(ch2) {
             case 1:
-                morpion(rep);
+                morpion(rep, id);
                 break;
                 
             case 2:
-                //bataille navale
+                bataillenavale(rep, id);
                 break;
                 
             case 3:
-                //blackjack
+                jack(rep);
                 break;
                 
             case 4:
-                liarDice(rep);
+                liarDice(rep, id);
                 break;
                 
             case 5:
@@ -90,4 +90,41 @@ int choiceGameMenu(joueur rep[], int id) {
     }
     while(ch2 != 5);
     return id;
+}
+
+void tabScores(joueur rep[], int id) {
+    joueur classement[REP];
+    int i=0, j=0, k=0, l=0, temp=0, enter=0;
+    char tempo[NOM];
+    memset(tempo, 0, NOM);
+    for(l=0; l < REP; l++) {
+        classement[l]=rep[l];
+    }
+    for (j=0 ; j < REP ; j++) {
+        for(i=j+1 ; i < REP ; i++) {
+            if (classement[i].score > classement[j].score) 
+            {
+                temp=classement[i].score;
+                strcpy(tempo, classement[i].nom);
+                classement[i].score=classement[j].score;
+                strcpy(classement[i].nom, classement[j].nom);
+                classement[j].score=temp;
+                strcpy(classement[j].nom, tempo);
+            }
+        }
+    }
+    system("cls");
+    locate(40, 0);
+    printf("**** Tableau des scores ****");
+    for (k=0 ; k < id ; k++) {
+        locate(32, 2*k+3);
+        printf("#%d %s", k+1, classement[k].nom);
+        locate(71, 2*k+3);
+        printf("%d", classement[k].score);
+    }
+    locate(38, 2*id+5);
+    printf("Appuyer sur [Enter] pour continuer.");
+    fflush(stdin);
+    while (enter != '\r' && enter != '\n') { enter = getchar(); }
+    enter=0;
 }
